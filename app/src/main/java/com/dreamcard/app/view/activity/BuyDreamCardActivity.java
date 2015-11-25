fc685d5959efa2c5c885db0a50fb804467891088
WIN <walid511@hotmail.com>
2015-11-25 21:37:16 +0200

    add where to buy dream card activity as list, and stores as list

--- app/src/main/java/com/dreamcard/app/view/activity/BuyDreamCardActivity.java
+++ app/src/main/java/com/dreamcard/app/view/activity/BuyDreamCardActivity.java
@@ -1,10 +1,13 @@
 package com.dreamcard.app.view.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.dreamcard.app.R;
@@ -12,9 +15,15 @@ import com.dreamcard.app.constants.Params;
 import com.dreamcard.app.constants.ServicesConstants;
 import com.dreamcard.app.entity.ErrorMessageInfo;
 import com.dreamcard.app.entity.MessageInfo;
+import com.dreamcard.app.entity.ServiceRequest;
+import com.dreamcard.app.entity.Stores;
+import com.dreamcard.app.services.AllBusinessAsync;
 import com.dreamcard.app.services.ContentBySlagAsync;
 import com.dreamcard.app.services.GetInvoiceForOfferAsync;
+import com.dreamcard.app.view.adapters.SellingAreasAdapter;
 import com.dreamcard.app.view.interfaces.IServiceListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by WIN on 11/17/2015.
@@ -22,41 +31,55 @@ import com.dreamcard.app.view.interfaces.IServiceListener;
 public class BuyDreamCardActivity extends Activity
     implements IServiceListener {
 
-    private ContentBySlagAsync sellingLocationAsync;
-    private TextView sellingLocationTxt;
+    private static final String LOG_TAG = BuyDreamCardActivity.class.getName();
+
+    private ListView list;
+    private ProgressBar progressBar;
+    private TextView noMatchs;
+
+    private AllBusinessAsync businessAsync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_dream_card);
 
-        sellingLocationTxt = (TextView) findViewById(R.id.txt_selling_locations);
-        sellingLocationAsync = new ContentBySlagAsync(this
-                , ServicesConstants.getContentSlagRequestParams(Params.ABOUT_US_SLAG)
-                , Params.SERVICE_PROCESS_1);
-        sellingLocationAsync.execute(this);
+        list = (ListView) findViewById(R.id.where_to_buy_list);
+        progressBar = (ProgressBar) findViewById(R.id.buy_dream_progress);
+        progressBar.setVisibility(View.VISIBLE);
+        noMatchs = (TextView) findViewById(R.id.buy_dream_no_matches);
+
+        businessAsync = new AllBusinessAsync(this, new ArrayList<ServiceRequest>(), Params.SERVICE_PROCESS_1);
+        businessAsync.execute(this);
     }
 
 
     @Override
     public void onServiceSuccess(Object b, int processType) {
-        Log.i(OfferInvoicePdfActivity.class.getName(), "OnServiceSucceeded with: " + b.toString());
+        Log.i(LOG_TAG, "OnServiceSucceeded with: " + b.toString());
         if (b != null) {
-            MessageInfo info= (MessageInfo) b;
-            String toShow = info.getValue();
-            toShow = toShow.replaceAll("(\\\\r\\\\n|\\\\n)", "<br />");
-            sellingLocationTxt.setText(Html.fromHtml(toShow));
+            SellingAreasAdapter adapter = new SellingAreasAdapter(this, (ArrayList<Stores>) b);
+            list.setAdapter(adapter);
+            progressBar.setVisibility(View.GONE);
+            return;
         }
+
+        progressBar.setVisibility(View.GONE);
+        noMatchs.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onServiceFailed(ErrorMessageInfo info) {
+        progressBar.setVisibility(View.GONE);
+        noMatchs.setVisibility(View.VISIBLE);
     }
 
     @Override
-    protected void onPause() {
-        sellingLocationAsync.cancel(true);
-        super.onPause();
+    protected void onDestroy() {
+        if (businessAsync != null && businessAsync.getStatus() == AsyncTask.Status.RUNNING) {
+            businessAsync.cancel(true);
+        }
+        super.onDestroy();
     }
 }
 
